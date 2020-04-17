784a5280d8ef89b8a773ae933f1be859f8439e2c
Ronnakorn Hompoa <kornkung.h@Ronnakorns-MacBook-Pro.local>
2020-04-18 00:07:49 +0700

    Updates

---
+++ precache-manifest.c7258f85b7428ae3636ef551b220cd8c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2696caa0df1787ee390a9bd6e0073361",
+    "url": "/port-folio/index.html"
+  },
+  {
+    "revision": "ea092cf85b869cb97b17",
+    "url": "/port-folio/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "b15d893cead4f1e9dbb7",
+    "url": "/port-folio/static/css/main.b25628e7.chunk.css"
+  },
+  {
+    "revision": "ea092cf85b869cb97b17",
+    "url": "/port-folio/static/js/2.008a5cd6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/port-folio/static/js/2.008a5cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15d893cead4f1e9dbb7",
+    "url": "/port-folio/static/js/main.5e024efd.chunk.js"
+  },
+  {
+    "revision": "9708e2d264b29fa4cca7",
+    "url": "/port-folio/static/js/runtime-main.1f9cc217.js"
+  },
+  {
+    "revision": "b758307213819cb530704dc44448baca",
+    "url": "/port-folio/static/media/Picture1.b7583072.png"
+  },
+  {
+    "revision": "9735a9aef00954d47b456b9916abb0fa",
+    "url": "/port-folio/static/media/Picture2.9735a9ae.png"
+  },
+  {
+    "revision": "001cad62733c31b55514f2fd3c1bac87",
+    "url": "/port-folio/static/media/Picture3.001cad62.png"
+  },
+  {
+    "revision": "b229dadc94a3cf437f5750ef4a65454b",
+    "url": "/port-folio/static/media/profileImg.b229dadc.jpg"
+  },
+  {
+    "revision": "79b98c6bb313033bdac643c88840eb85",
+    "url": "/port-folio/static/media/scg-logo.79b98c6b.jpg"
+  }
+]);
